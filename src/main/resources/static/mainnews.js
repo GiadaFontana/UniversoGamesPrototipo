948f10fb749ce0aaa268b013b2188adda0d0b193
Giada Fontana <giadafontana988@gmail.com>
2020-06-11 09:45:46 +0200

    parte nuova html

--- src/main/resources/static/mainnews.js
+++ src/main/resources/static/mainnews.js
@@ -13,16 +13,17 @@ $(document).ready(function() {
 						<td>
 							<button class='detail-button' data-id='${res[i].id}'>Dettaglio</button>
 							<button class='delete-button' data-id='${res[i].id}'>Delete</button>
+
 						</td>
 					</tr>`)
-					.appendTo("#lista_news");
+					.appendTo("#lista-news");
 			}
 		})
 	}
 	
 	getNews()
 	
-		$('#lista_news').on('click', '.detail-button', function() {
+		$('#lista-news').on('click', '.detail-button', function() {
 		console.log('ciao')
 		const id = $(this).attr('data-id')
 		$('#detail-news-modal').css('display', 'block')
@@ -44,7 +45,7 @@ $(document).ready(function() {
 		$("#new-detail").html('')
 	})
 	
-	$('#lista_news').on('click', '.delete-button', function() {
+	$('#lista-news').on('click', '.delete-button', function() {
 		const id = $(this).attr('data-id')
 		deleteNews(id, $(this).parent().parent())
 	})
@@ -58,6 +59,83 @@ $(document).ready(function() {
 			}
 		})
 	}
+
+	$('#open-add-news').click(function () {
+		console.log('porcaputtana')
+		$('#add-news-modal').show('slow')
+		
+	})
+
+	const categorie = [];
+	function getCategorie() {
+        $.get('news', function (res) {
+            for (let i = 0; i < res.length; i++) {
+            categorie.push(res[i].categoria)
+            }
+        })
+    }
 	
-	
+
+    const categorieCensite = []
+	function censisciCategorie(){
+        categorieCensite.splice(0, categorieCensite.length);
+        for (const categoria of categorie){
+            let esiste = false; 
+            if (categorieCensite.length == 0){
+                categorieCensite.push(categoria);
+            } else {
+                for (const categoria2 of categorieCensite){
+                    if (categoria === categoria2){ 
+                        esiste = true;
+                        break
+                     }
+                } if (!esiste){
+                    categorieCensite.push(categoria);
+                }    
+            }
+        }
+        console.log(categorieCensite);
+        
+	}
+    
+    $("#close-add-news").on("click", function() {
+		$("#add-news-modal").hide('slow')
+	})
+    
+	    $('#add-news').click(function() {
+		const n = {
+				titolo: $('#titolo').val(), 
+				categoria: $('#categoria').val(),
+				contenuto: $('#contenuto').val(), 
+				datapubblicazione: $('#datapubblicazione').val(), 
+				autore: $('#autore').val()
+				
+		}
+		
+		addNews(n)
+
+        $('#titolo').val('')
+        $('#categoria').val('')
+        $('#contenuto').val('')
+        $('#datapubblicazione').val('')
+        $('#autore').val('')
+        $('#lista-news').html('')
+         getNews()
+        $('#add-news-modal').hide('slow')
+       
+	   
+	  })
+		
+		
+		function addNews(n){
+		$.ajax({
+			type: 'POST',
+			url: '/news',
+			data: JSON.stringify(n),
+			contentType: "application/json",
+			dataType: 'json',
+			success: function(data) {
+			}
+	    })
+     }
 })
--- src/main/resources/static/mainrecensioni.js
+++ src/main/resources/static/mainrecensioni.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
 	
-	function getRecensioni() {
-		
-		
+	function getRecensioni() {	
 		$.get("recensioni", function(res) {
-			
-			
 			for (let i = 0; i < res.length; i++) {
 				$(`<tr>
 						<td>${res[i].titoloVideogioco}</td>
@@ -14,16 +10,12 @@ $(document).ready(function() {
 							<button class='delete-button' data-id='${res[i].id}'>Delete</button>
 						</td>
 					</tr>`)
-					.appendTo("#lista_recensioni")
-					
+					.appendTo("#lista-recensioni")
 			}
 		})
 	}
-	
 	getRecensioni() 
-	
-	
-	$('#lista_recensioni').on('click', '.detail-button', function() {
+	$('#lista-recensioni').on('click', '.detail-button', function() {
 		console.log('ciao')
 		const id = $(this).attr('data-id')
 		$('#detail-recensione-modal').css('display', 'block')
@@ -40,16 +32,14 @@ $(document).ready(function() {
 			).appendTo("#recension-detail")
 		})
 	}
-	
 	$("#close-detail").on("click", function() {
 		$("#detail-recensione-modal").css('display','none')
 		$("#recension-detail").html('')
 	})
-	$('#lista_recensioni').on('click', '.delete-button', function() {
+	$('#lista-recensioni').on('click', '.delete-button', function() {
 		const id = $(this).attr('data-id')
 		deleteRecensione(id, $(this).parent().parent())
 	})
-	
 	function deleteRecensione(id, htmlRow) {
 		$.ajax({
 			url: `recensioni/${id}`,
@@ -59,5 +49,54 @@ $(document).ready(function() {
 			}
 		})
 	}
+
+	$('#open-add-recensioni').click(function () {
+		console.log('boh')
+		$('#add-recensioni-modal').show('slow')
+		
+	})
+
+
+    
+    $("#close-add-recensioni").on("click", function() {
+		$("#add-recensioni-modal").hide('slow')
+	})
+    
+	    $('#add-recensioni').click(function() {
+		const r = {
+				titoloVideogioco: $('#titoloVideogioco').val(), 
+				dataRecensione: $('#dataRecensione').val(),
+				recensione: $('#recensione').val(), 
+				punteggio: $('#punteggio').val(), 
+				recensore: $('#recensore').val()
+				
+		}
+		
+		addRecensioni(r)
+
+        $('#titoloVideogioco').val('')
+        $('#dataRecensione').val('')
+        $('#recensione').val('')
+        $('#punteggio').val('')
+        $('#recensore').val('')
+        $('#lista-recensioni').html('')
+         getRecensioni()
+        $('#add-recensioni-modal').hide('slow')
+       
+	   
+	  })
+		
+		
+		function addRecensioni(r){
+		$.ajax({
+			type: 'POST',
+			url: '/recensioni',
+			data: JSON.stringify(r),
+			contentType: "application/json",
+			dataType: 'json',
+			success: function(data) {
+			}
+	    })
+     }
 
 })
